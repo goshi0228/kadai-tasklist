ce3ab189d91b9257c4c6b7208ac8ff14cf3155af
goshi.nakamura <goshi.nakamura.0228@gmail.com>
2021-02-27 14:29:34 +0000

    Re1 task

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
  delete 'logout', to: 'sessions#destroy'
  
  get 'signup', to: 'users#new'
+ 
  resources :users, only: [:index, :show, :create]
+  
+ resources :tasks, only: [:create, :destroy]
 end
